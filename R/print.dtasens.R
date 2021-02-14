e2c723f0661034186c927b15f5aa21e76409e332
Yi <Yi@Yi-2.local>
2021-02-14 22:42:43 +0900

    update all

--- R/print.dtasens.R
+++ R/print.dtasens.R
@@ -1,13 +1,22 @@
-
+#' Print DTAsens
+#'
+#' @description Print DTAsens
+#'
+#' @param x object
+#' @param digits digits
+#' @param ... graphical parameters to plot
+#'
 #' @examples
+#'
 #' (opt1 <- dtasens1(IVD, p = 0.7))
 #' (opt2 <- dtasens2(IVD, p = 0.7))
+#'
+#' @rdname print.DTAsens
+#' @export
+print.DTAsens <- function(x, digits = 3, ...){
 
+  if(!inherits(x, "DTAsens")) stop("Only valid for dtasens1 or dtasens2")
 
-print.DTAsens <- function(object, digits = 3, ...){
-
-  if(!inherits(object, "DTAsens")) stop("Only valid for dtasens1 or dtasens2")
-
-  print(list(par = object$par, ci = object$ci), ...)
+  print(list(par = x$par, ci = x$ci), ...)
 
 }
--- R/sAUC.ci.R
+++ R/sAUC.ci.R
@@ -103,17 +103,23 @@ sAUC.ci <- function(object, B = 10, ncores, type = "SOCK", ci.level = 0.95,
 
 }
 
+#' Print sAUC CI
+#'
+#' @description Print sAUC CI
+#'
+#' @param x object
+#' @param digits digits
+#' @param ... graphical parameters to plot
+#'
+#' @rdname print.sAUC.ci
+#' @export
+print.sAUC.ci <- function(x, digits = 3, ...){
 
-print.sAUC.ci <- function(object, digits = 3, ...){
+  if(!inherits(x, "sAUC.ci")) stop("Only valid for sAUC.ci")
 
-  if(!inherits(object, "sAUC.ci")) stop("Only valid for sAUC.ci")
-
-  sAUC <- c(object$sAUC, object$CI.L, object$CI.U)
+  sAUC <- c(x$sAUC, x$CI.L, x$CI.U)
   names(sAUC) <-c("sAUC", "CI.L", "CI.L")
 
-  args <- list(x = sAUC, digits = digits, ...)
-  do.call("print", args, ...)
-
-  #NextMethod("print")
+  print(sAUC, digits = digits, ...)
 
 }
