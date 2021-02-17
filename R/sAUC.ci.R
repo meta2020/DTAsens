32b67a0df4c416b0dacd8cad075ac616985309be
Yi <Yi@Yi-2.local>
2021-02-17 13:12:45 +0900

    update

--- R/sAUC.ci.R
+++ R/sAUC.ci.R
@@ -8,6 +8,10 @@
 #' @param type c1
 #' @param ci.level par
 #' @param hide.progress par
+#' @param plot.ROC par
+#' @param add.sum.point add.sum.point
+#' @param roc.col roc.col
+#' @param ... ...
 #'
 #' @return convergence list
 #'
@@ -23,12 +27,17 @@
 #' opt1 <- dtasens1(IVD, p = 0.7)
 #' (ci <- sAUC.ci(opt1, B = 5))
 #'
-sAUC.ci <- function(object, B = 10, ncores, type = "SOCK", ci.level = 0.95,
-                    hide.progress = FALSE)
+sAUC.ci <- function(object, B = 10,
+                    ncores, type = "SOCK", ci.level = 0.95,
+                    hide.progress = FALSE,
+                    plot.ROC = FALSE,
+                    add.sum.point = FALSE,
+                    roc.col = 1,
+                    ...)
 {
-  if(!requireNamespace("foreach")) install.packages("foreach")   else require(foreach)
-  if(!requireNamespace("parallel")) install.packages("parallel") else require(parallel)
-  if(!requireNamespace("doSNOW")) install.packages("doSNOW")     else require(doSNOW)
+  if(!requireNamespace("foreach")) install.packages("foreach")   else requireNamespace("foreach")
+  if(!requireNamespace("parallel")) install.packages("parallel") else requireNamespace("parallel")
+  if(!requireNamespace("doSNOW")) install.packages("doSNOW")     else requireNamespace("doSNOW")
 
   if(!inherits(object, "DTAsens")) stop("Only valid for dtasens1 or dtasens2")
 
@@ -55,7 +64,7 @@ sAUC.ci <- function(object, B = 10, ncores, type = "SOCK", ci.level = 0.95,
 
   if(object$func.name == "dtasens1"){
 
-    sauc.t <- foreach(r=1:B, .combine=c, .packages="DTAsens", .options.snow = opts) %dopar% {
+    par <- foreach(r=1:B, .combine=c, .packages="DTAsens", .options.snow = opts) %dopar% {
 
       y1.t <- sapply(1:S, function(i) rnorm(1,y1[i],v1[i]))
       y2.t <- sapply(1:S, function(i) rnorm(1,y2[i],v2[i]))
@@ -64,14 +73,14 @@ sAUC.ci <- function(object, B = 10, ncores, type = "SOCK", ci.level = 0.95,
 
       args <- c(list(data = data.t), object$pars.info)
       opt1.t <- do.call("dtasens1", args)
-      opt1.t$par[11]
+      opt1.t$par[c(1:5, 11)]
 
     }
   }
 
   if(object$func.name == "dtasens2"){
 
-      sauc.t <- foreach(r=1:B, .combine=c, .packages="DTAsens", .options.snow = opts) %dopar% {
+      par <- foreach(r=1:B, .combine=c, .packages="DTAsens", .options.snow = opts) %dopar% {
 
         y1.t <- sapply(1:S, function(i) rnorm(1,y1[i],v1[i]))
         y2.t <- sapply(1:S, function(i) rnorm(1,y2[i],v2[i]))
@@ -79,7 +88,7 @@ sAUC.ci <- function(object, B = 10, ncores, type = "SOCK", ci.level = 0.95,
         data.t <- data.frame(y1 = y1.t, y2 = y2.t, v1 = v1, v2 = v2)
         args <- c(list(data = data.t), object$pars.info)
         opt2.t <- do.call("dtasens2", args)
-        opt2.t$par[11]
+        opt2.t$par[c(1:5, 11)]
 
     }
   }
@@ -87,9 +96,20 @@ sAUC.ci <- function(object, B = 10, ncores, type = "SOCK", ci.level = 0.95,
   if (!hide.progress) close(pb)
   parallel::stopCluster(cl)
 
-  q1 <- quantile(scale(sauc.t), probs = (1-ci.level)/2)
-  q2 <- quantile(scale(sauc.t), probs = 1-(1-ci.level)/2)
-  se <- sd(sauc.t)
+  PAR <- matrix(par, nrow = 6)
+  sauc.t   <- PAR[6,]
+
+  se <- sd(sauc.t, na.rm = TRUE)
+  sl.sauc.t <- (sauc.t - mean(sauc.t, na.rm = TRUE))/se
+
+  s.sauc.r <- order(sl.sauc.t)
+  PAR.r <- PAR[, s.sauc.r]
+
+  s.sauc.t <- sort(sl.sauc.t)
+
+  q1 <- quantile(s.sauc.t, probs = (1-ci.level)/2, na.rm = TRUE)
+  q2 <- quantile(s.sauc.t, probs = 1-(1-ci.level)/2, na.rm = TRUE)
+
   sauc <- unlist(object$auc.all$value)
 
   list <- list(sAUC = sauc,
@@ -98,6 +118,16 @@ sAUC.ci <- function(object, B = 10, ncores, type = "SOCK", ci.level = 0.95,
        cluster = cl)
 
   class(list) <- "sAUC.ci"
+
+  if(plot.ROC){
+
+    q1.pos <- which.min(abs(s.sauc.t - q1))
+    q2.pos <- which.min(abs(s.sauc.t - q2))
+
+    msROC(PAR.r[, q1.pos:q2.pos], add.sum.point = add.sum.point, ncols = rep("grey", B))
+    sROC(object, add = TRUE, add.sum.point = add.sum.point, roc.col = roc.col,...)
+
+  }
 
   list
 
